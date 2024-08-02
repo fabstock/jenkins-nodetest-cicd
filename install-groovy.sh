8f644041843fe281fa3109c59439b5110ec71e74
Fab Stock <fabstock@stock.eu.org>
2024-08-02 19:50:14 +0200

    20240802-19:50:14 unzip -o alpine

--- install-groovy.sh
+++ install-groovy.sh
@@ -3,6 +3,6 @@
 VERSION="4.0.12"
 
 curl  https://groovy.jfrog.io/artifactory/dist-release-local/groovy-zips/apache-groovy-binary-${VERSION}.zip -O apache-groovy-binary-${VERSION}.zip
-unzip  -f apache-groovy-binary-4.0.12.zip
+unzip  -o apache-groovy-binary-4.0.12.zip
 ./groovy-4.0.12/bin/groovy --version
 
